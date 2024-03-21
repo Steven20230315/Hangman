12d85ed36a9fb84b6166bf196a3fd779f42e67ef
Steven20230315 <125541670+Steven20230315@users.noreply.github.com>
2024-03-21 12:30:09 +0800

    feat: Create new component to visualize game process
    - Introduced the `HangmanDrawing` component that dynamically renders the
    hangman's figure based on the number of incorrect guesses.

---
+++ src/components/HangmanDrawing.tsx
@@ -0,0 +1,33 @@
+import Gallows from '../assets/wrappers/Gallows.ts';
+
+type HangmanDrawingProps = {
+	numberOfIncorrectGuesses: number;
+};
+
+const HEAD = (
+	<div className='head'>
+		<div className='right-eye eye' />
+		<div className='left-eye eye' />
+		<div className='smile' />
+	</div>
+);
+const BODY = <div className='body' />;
+const RIGHT_ARM = <div className='right-arm limbs' />;
+const LEFT_ARM = <div className='left-arm limbs' />;
+const RIGHT_LEG = <div className='right-leg limbs' />;
+const LEFT_LEG = <div className='left-leg limbs' />;
+const BODY_PARTS = [HEAD, BODY, RIGHT_ARM, LEFT_ARM, RIGHT_LEG, LEFT_LEG];
+
+export default function HangmanDrawing({
+	numberOfIncorrectGuesses,
+}: HangmanDrawingProps) {
+	return (
+		<Gallows $backgroundColor='black' $base={10}>
+			{BODY_PARTS.slice(0, numberOfIncorrectGuesses)}
+			<div className='rope'></div>
+			<div className='top-bar'></div>
+			<div className='pillar'></div>
+			<div className='base'></div>
+		</Gallows>
+	);
+}
