6001d86f0c5e0d884f02fe6e0765c09707ec9e08
Steven20230315 <125541670+Steven20230315@users.noreply.github.com>
2024-03-21 11:51:06 +0800

    feat: Add Keyboard input handling for guessing word
    - Added a `useEffect` for keyboard input handling
        - Invokes `handleAddGuessedLetter` when the key pressed is a letter
        - Removes event listener when the component unmounts to prevent
        memory leak
    
    - Added a `handleAddGuessedLetter` function for updating `guessedLetters`
        - Only updates `guessedLetters` when the letter is not already in the
        `guessedLetters`
        - Both `useEffect` and `Keyboard` component use `handleAddGuessedLetter`
        to update `guessedLetters`, so it should not be defined in `useEffect`
    
    - Included a `useCallback` to `handleAddGuessedLetter` function.
        - To reduce unnecessary re-renders

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,33 @@ function App() {
 	useEffect(() => {
 		setWord(wordGenerator());
 	}, []);
+	// useCallback to prevent unnecessary re-renders.
+	// Since this function is included in the dependency array of the useEffect hook, you need to use useCallback to prevent unnecessary re-renders.
+	const handleAddGuessedLetter = useCallback(
+		(letter: string) => {
+			// If the letter has already been guessed, ignore it. Else, add it to guessedLetters. This is to prevent duplicates letters in the guessedLetters array.
+			if (guessedLetters.includes(letter)) return;
+			setGuessedLetters((prev) => [...prev, letter]);
+		},
+		[guessedLetters]
+	);
+
+	useEffect(() => {
+		const handler = (e: KeyboardEvent) => {
+			const key = e.key;
+			// If it's not a letter, ignore it
+			if (!key.match(/^[a-z]$/)) return;
+
+			e.preventDefault();
+			handleAddGuessedLetter(key);
+		};
+		document.addEventListener('keypress', handler);
+		// This will clean up the event listener when the component unmounts to prevent memory leaks. 
+		// The event listener will not be replaced by adding a new one. If you didn't remove the event listener, there will be multiple event listeners attached to the elements.
+		return () => {
+			document.removeEventListener('keypress', handler);
+		};
+	}, [handleAddGuessedLetter]);
 	return (
 		<>
 			<h1>Hi</h1>
