503fa96fc35757d11d1bbad0ce284c0cc6e17c59
Steven20230315 <125541670+Steven20230315@users.noreply.github.com>
2024-03-21 11:45:58 +0800

    feat: Integrate word generation and letter tracking
    - 'word' is the current word being guessed
    - 'guessedLetters' is an array of guessed letters
    - 'incorrectLetters' is an array of incorrect guessed letters. It's
    derived from 'guessedLetters', filtering out letters that are not in the
    current word.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,18 @@
 function App() {
+	const [word, setWord] = useState('');
+
+	// Track the letters guessed by the player
+	const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
+	// From guessedLetters, filter out the guessed letters that are not in the word.
+	//
+	const incorrectLetters = guessedLetters.filter(
+		(letter) => !word.includes(letter)
+	);
+	// I'm not sure if I need this
+	useEffect(() => {
+		setWord(wordGenerator());
+	}, []);
 	return (
 		<>
 			<h1>Hi</h1>
