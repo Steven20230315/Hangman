ca9a87203488568d8c2b37e65ae74457840f2290
Steven20230315 <125541670+Steven20230315@users.noreply.github.com>
2024-03-25 16:40:38 +0800

    feat: setup TypeScript with Node.js and auto-restart server on changes
    - Added 'ts-node',"@types/cors","@types/express","@types/node",
    "nodemon","ts-node-dev",and "typescript" as dev dependencies.
    
    - Configured package.json:
      - Added a 'build' script for compiling TypeScript files from
      src/server.ts to dist/server.js.
      - Configured 'nodemon' to watch for changes in TypeScript files,
      recompile them using the 'build' script, and restart the server
      automatically.

---
+++ dist/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import dotenv from 'dotenv';
+//For env File 
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 8000;
+app.get('/', (req, res) => {
+    res.send('Welcome to Express & TypeScript Server!!!');
+});
+app.listen(port, () => {
+    console.log(`Server is Fire at http://localhost:${port}`);
+});
+//# sourceMappingURL=server.js.map
